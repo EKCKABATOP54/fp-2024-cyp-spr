5f0bff11ec949214d64292d42ded02cd12869b25
EKCKABATOP54 <androposhtar1029@gmail.com>
2024-04-01 23:48:02 +0200

    Done

--- hw07/src/Expr.hs
+++ hw07/src/Expr.hs
@@ -1,27 +1,42 @@
+--{-# LANGUAGE GADTs #-}
+
 module Expr where 
-import StateDemo ( State, execState, get, modify )
-import Data.Maybe ( fromJust )
 
-data Expr = V String | C Int | Plus Expr Expr 
-          | Let String Expr Expr 
-        
-type ExprState = [(String, Int)]
+  import Text.Printf (printf)
 
-eval :: Expr -> State ExprState Int
-eval (V v) = do 
-  env <- get 
-  return $ fromJust $ lookup v env
-eval (C x) = return x 
-eval (Plus x y) = do 
-  x <- eval x 
-  y <- eval y 
-  return $ x + y 
-eval (Let x v b) = do 
-  v <- eval v 
-  modify ((x, v) :)
-  eval b 
+  data Expr a = Const a 
+        | SquareRoot (Expr a)
+        | Var String
+        | Binop BinOptor (Expr a) (Expr a)
+        deriving Eq
+ 
+  {-
+  data Expr a where
+    Const :: Num a => a -> Expr a
+    SquareRoot :: Num a => Expr a -> Expr a
+    Binop :: Num a => BinOptor -> Expr a -> Expr a -> Expr a
+    Var :: String -> Expr a
+  -}
 
--- runEval (Let "x" (C 13) (Plus (V "x") (C 42)))
-runEval :: Expr -> Int
-runEval expr = 
-  execState (eval expr) [] 
+  data BinOptor = Plus | Minus | Mult | Div | Pow deriving Eq
+
+  instance Num a => Num (Expr a) where
+    (+) = Binop Plus
+    (*) = Binop Mult
+    negate = Binop Minus (Const 0) 
+    fromInteger = Const . fromInteger
+
+  
+  instance Show BinOptor where
+    show Plus = "+"
+    show Minus = "-"
+    show Mult = "*"
+    show Div = "/"
+    show Pow = "^"
+
+  instance Show a => Show (Expr a) where 
+    show e = case e of
+            (Const c)       -> show c
+            (SquareRoot e1)  -> printf "sqrt(%s)" (show e1)
+            (Binop op e1 e2) -> printf "(%s %s %s)" (show e1) (show op) (show e2)
+            (Var v)         -> printf "%s" (show v)
--- hw07/test/Spec.hs
+++ hw07/test/Spec.hs
@@ -1,2 +1,35 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+import Expr
+import Error
+import Eval
+import Test.Tasty
+import Test.Tasty.HUnit
+import StateDemo
+import qualified Data.Map.Strict as M
+import Control.Exception (assert)
+
+
+
+evalTests :: TestTree
+evalTests = testGroup "Eval tests"
+  [ testCase "Base test1" $ assertEqual "eval on number is number" (execState (eval (Const 777))M.empty) (Right 777)
+  , testCase "Base test2" $ assertEqual "" (execState (eval (Binop Pow (Binop Div (Binop Mult (Binop Minus (Binop Pow (Binop Plus (SquareRoot(Const 25)) (Const 3)) (Const 2)) (Binop Mult (Const 4) (Const 7))) (Const 2)) (Const 5)) (Const 3)) )M.empty) (Right ( ((  ( ( sqrt 25 + 3)**2 - 4 * 7) * 2) / 5)**3))
+  , let e1 = Binop Mult (Const 4) (Const 5)
+        e2 = Binop Plus (Const 1) (Const (-1)) in
+    testCase "Zero division test" $ assertEqual "" (execState (eval (Binop Div e1 e2)) M.empty) ( Left (DivisionByZero e1 e2))
+  , let e = Binop Minus (Const 1) (Const 100) in
+  
+    testCase "RootOfNegative error" $ assertEqual "" (execState (eval (SquareRoot e)) M.empty) (Left $ RootOfNegative e)
+  , testCase "Var test" $ assertEqual "" (execState (eval (Var "x")) $ M.fromList [("x", 100)]) (Right 100)
+
+  
+  , testCase "Undefined variable" $ assertEqual "" (execState (eval (Binop Plus (Var "z") (Var "y"))) $ M.fromList [("x", 10), ("y", 20)]) ( Left $ UndefinedVariable "z")
+  , testCase "Base test 3" $ assertEqual "" (execState (eval (Binop Minus (Var "x") (Var "x"))) $ M.fromList [("x", 0)]) (Right 0)
+  , testCase "Pow test 0^0" $ assertEqual "" (execState (eval (Binop Pow (Var "x") (Var "y"))) $ M.fromList [("x", 0), ("y", 0)]) (Right 1)
+  ]
+
+exprTests :: TestTree
+exprTests = testGroup "Expr tests" [evalTests]
+
+main :: IO()
+main = defaultMain exprTests
